a7d7d82f8ba6d0d7a49581d60ef8e2f1b4464ec6
Zachary <zacharyschrader@proton.me>
2025-07-20 10:15:45 -0500

    Exception raising passes

--- chapter_2/simple_messages.py
+++ chapter_2/simple_messages.py
@@ -2,5 +2,9 @@ stored_message = ""
 
 def set_message(message):
     global stored_message
+
+    if message == "":
+        raise ValueError("Invalid message value")
+    
     stored_message = message
     print(f"Message '{message}' stored") 
